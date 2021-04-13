72e30bea523a179a04ae8cdc029c9eb30dec5d5f
Cory Bass <cory.bass.07@gmail.com>
2021-04-13 14:32:49 -0500

    added icon to start on delete in crud

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -4,24 +4,46 @@ import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 // context
 import { Context } from "../context/BlogContext";
 
+// icons
+import { Feather } from "@expo/vector-icons";
+
 const IndexScreen = () => {
     const { state, addBlogPost } = useContext(Context);
 
     return (
         <View>
-            <Text>Index Screen</Text>
             <Button title="Add Post" onPress={addBlogPost} />
             <FlatList
                 data={state}
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({ item }) => {
-                    return <Text>{item.title}</Text>;
+                    return (
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <Feather name="trash" style={styles.icon} />
+                        </View>
+                    );
                 }}
             />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderBottomWidth: 1,
+        borderColor: "gray",
+    },
+    title: {
+        fontSize: 18,
+    },
+    icon: {
+        fontSize: 24,
+    },
+});
 
 export default IndexScreen;
