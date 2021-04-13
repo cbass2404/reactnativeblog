7c0727a5a1b73c7a4f578813e54c0d02fd3528a4
Cory Bass <cory.bass.07@gmail.com>
2021-04-13 16:02:35 -0500

    initiate ability to edit post

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+import { EvilIcons } from "@expo/vector-icons";
 
 // context
 import { Context } from "../context/BlogContext";
@@ -14,8 +16,19 @@ const ShowScreen = ({ navigation }) => {
     return (
         <View>
             <Text>{blogPost.title}</Text>
+            <Text>{blogPost.content}</Text>
         </View>
     );
+};
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("Edit")}>
+                <EvilIcons name="pencil" size={35} />
+            </TouchableOpacity>
+        ),
+    };
 };
 
 const styles = StyleSheet.create({});
